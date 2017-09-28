d3872fec02ad031fe2a5de01ff5ff829e2f17842
Gabriel Pereira <gabrielp@backbase.com>
2017-09-28 17:13:30 +0200

    Adding find method to binary search tree

--- src/algorithms/binary-search/binary-search-tree.js
+++ src/algorithms/binary-search/binary-search-tree.js
@@ -10,6 +10,29 @@ class BinarySearchTree {
         this.compare = compare;
     }
 
+    /**
+     * @param value Any
+     * @returns BinarySearchNode | boolean
+     */
+    find(value, current = this.root) {
+        const comp = this.compare(value, current.value);
+
+        if (comp === 0)
+            return current;
+
+        if (comp > 0 && current.right)
+            return this.find(value, current.right);
+
+        if (comp < 0 && current.left)
+            return this.find(value, current.left);
+
+        return false;
+    }
+
+    /**
+     * @param value BinarySearchNode | Any
+     * @returns BinarySearchNode | boolean
+     */
     insert(value, current = this.root) {
         const node = value instanceof Node ? value : new Node(value);
 
--- test/algorithms/binary-search/binary-search-tree.spec.js
+++ test/algorithms/binary-search/binary-search-tree.spec.js
@@ -2,9 +2,12 @@ const Tree = require("../../../src/algorithms/binary-search/binary-search-tree")
 const assert = require('assert');
 
 describe('Binary Search Tree', () => {
-    it('It should insert a value in the correct place', () => {
-        const tree = new Tree((a, b) => a > b ? 1 : -1);
+    let tree,
+        compareFn = (a, b) => a === b ? 0 : a > b ? 1 : -1;
 
+    beforeEach(() => tree = new Tree(compareFn))
+
+    it('It should insert a value in the correct place', () => {
         const v1 = tree.insert(1);
         const v3 = tree.insert(3);
         const v4 = tree.insert(4);
@@ -14,5 +17,15 @@ describe('Binary Search Tree', () => {
         assert.equal(v1.right, v3);
         assert.equal(v1.left, vM1);
         assert.equal(v3.right, v4);
+    });
+
+    it('It should find the right node', () => {
+        const v1 = tree.insert(1);
+        const v3 = tree.insert(3);
+        const v4 = tree.insert(4);
+        const vM1 = tree.insert(-1);
+
+        assert.equal(tree.find(3), v3);
+        assert.equal(tree.find(-1), vM1);
     });
 });
